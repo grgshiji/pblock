c7b36751acf74248a72f52e0c7e2f87fc9e993f1
grgshiji <shiji@neoito.com>
2023-02-03 12:18:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const pblock = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello pblock`}))
+  res.end()
+  
+}
+
+export default pblock
